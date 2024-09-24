92e477f1bfee7f3b936c097865baca268b1bd033
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-24 13:09:00 -0400

    update

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Palindrome {
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    /**
+     * Returns true if the number is a palindrome.
+     *
+     * @param number the number to check
+     * @return true if a palindrome, false if not
+     */
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+
+        int numberClone = number;
+        int reverse = 0;
+
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
+            number /= 10;
+        }
+
+
+        return reverse == numberClone;
+    }
 }
+
